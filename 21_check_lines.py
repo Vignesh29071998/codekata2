a4642241e89e1110e9e311721933b5fa45da0ac3
Vignesh <46440651+Vignesh29071998@users.noreply.github.com>
2019-01-30 22:39:42 +0530

    Create 21_check_lines.py

---
+++ 21_check_lines.py
@@ -0,0 +1,9 @@
+a1,b1=map(int,input().split())
+a2,b2=map(int,input().split())
+a3,b3=map(int,input().split())
+if a1==a2==a3:
+  print('yes')
+elif b1==b2==b3:
+  print('yes')
+else:
+  print('no')
