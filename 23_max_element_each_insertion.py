adc6d90dd445339a52739fe7e66b72e6c7afab0d
Vignesh <46440651+Vignesh29071998@users.noreply.github.com>
2019-01-30 23:07:50 +0530

    Create 23_max_element_each_insertion.py

---
+++ 23_max_element_each_insertion.py
@@ -0,0 +1,12 @@
+n,k=map(int,input().split())
+n1=list(map(int,input().split()))
+k1=list(map(int,input().split()))
+k2=[]
+for i in k1:
+  n1.append(i)
+  k2.append(max(n1))
+for i in range(0,len(k2)):
+  if i==len(k2)-1:
+    print(k2[i])
+  else:
+    print(k2[i],end=' ')
